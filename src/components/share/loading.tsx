73f8bfd2937b81a784d40dc2068d56cffe8090f6
Huỳnh Thạnh Phú <phuhynhpen@gmail.com>
2026-06-29 00:21:02 +0700

    update code

--- src/components/share/loading.tsx
+++ src/components/share/loading.tsx
@@ -1,12 +1,29 @@
-import HashLoader from "react-spinners/HashLoader";
+import React from "react";
+import "./loading.css";
+import { DotLottiePlayer } from '@dotlottie/react-player';
+import '@dotlottie/react-player/dist/index.css';
 
 const Loading = () => {
-
-    const style: React.CSSProperties = { position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" };
+    const style: React.CSSProperties = { 
+        position: "fixed", 
+        top: "50%", 
+        left: "50%", 
+        transform: "translate(-50%, -50%)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+    };
 
     return (
         <div style={style}>
-            <HashLoader color="#36d7b7" />
+            <div className="lotus-loader-lottie" style={{ width: 180, height: 180 }}>
+                <DotLottiePlayer
+                    src="/logo/loading2.lottie"
+                    autoplay
+                    loop
+                />
+            </div>
         </div>
     )
 }
